9b7f6bbdf911856e415c8ee8f2ecdfa5aa4655e6
Aaron <azza1990uk@yahoo.com>
2018-09-18 22:16:14 +0100

    Add columns container

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,29 +17,31 @@ export default class IndexPage extends React.Component {
             </div>
             {posts
               .map(({ node: post }) => (
-                <div
-                  className="content column is-3"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <div className="thumbnail">
-                    <img src={post.frontmatter.thumbnail} />
+                <div className="collumns">
+                  <div
+                    className="content column is-3"
+                    style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                    key={post.id}
+                  >
+                    <div className="thumbnail">
+                      <img src={post.frontmatter.thumbnail} />
+                    </div>
+                    <p>
+                      <Link className="has-text-primary" to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                      <span> &bull; </span>
+                      <small>{post.frontmatter.date}</small>
+                    </p>
+                    <p>
+                      {post.excerpt}
+                      <br />
+                      <br />
+                      <Link className="button is-small" to={post.fields.slug}>
+                        Keep Reading →
+                      </Link>
+                    </p>
                   </div>
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
                 </div>
               ))}
           </div>
