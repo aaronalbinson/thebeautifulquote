739a5bc33eb402d794dfa6af5af74e85a6c87bba
Aaron <azza1990uk@yahoo.com>
2018-09-18 22:20:07 +0100

    Fix columns spell error and placement

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,35 +15,35 @@ export default class IndexPage extends React.Component {
             <div className="content">
               <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
             </div>
+            <div className="columns">
             {posts
               .map(({ node: post }) => (
-                <div className="collumns">
-                  <div
-                    className="content column is-3"
-                    style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                    key={post.id}
-                  >
-                    <div className="thumbnail">
-                      <img src={post.frontmatter.thumbnail} />
-                    </div>
-                    <p>
-                      <Link className="has-text-primary" to={post.fields.slug}>
-                        {post.frontmatter.title}
-                      </Link>
-                      <span> &bull; </span>
-                      <small>{post.frontmatter.date}</small>
-                    </p>
-                    <p>
-                      {post.excerpt}
-                      <br />
-                      <br />
-                      <Link className="button is-small" to={post.fields.slug}>
-                        Keep Reading →
-                      </Link>
-                    </p>
+                <div
+                  className="content column is-3"
+                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                  key={post.id}
+                >
+                  <div className="thumbnail">
+                    <img src={post.frontmatter.thumbnail} />
                   </div>
-                </div>
+                  <p>
+                    <Link className="has-text-primary" to={post.fields.slug}>
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <small>{post.frontmatter.date}</small>
+                  </p>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button is-small" to={post.fields.slug}>
+                      Keep Reading →
+                    </Link>
+                  </p>
+                </div> 
               ))}
+              </div>
           </div>
         </section>
       </Layout>
