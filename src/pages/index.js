d57831ccd2b5ac9a00c610acd66355b03affd2c2
Aaron <azza1990uk@yahoo.com>
2018-11-19 22:52:44 +0000

    Remove container on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default class IndexPage extends React.Component {
     return (
       <Layout>
         <section className="section">
-          <div className="container">
+          <div className="">
             <div className="content">
               {/* <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1> */}
             </div>
