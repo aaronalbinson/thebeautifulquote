8fbdd27cf7c770a91f29e3cfe56851cf8fd40e3c
Aaron <a.albinson@teluxhd.com>
2018-11-21 17:44:48 +0000

    Add footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+require("./Footer.scss");
+
 const Footer = () => (
   <footer className="kilimanjaro_area">
     <div className="foo_top_header_one section_padding_100_70">
